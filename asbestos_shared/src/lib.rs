2fdebf66b04faa70053d4d6234be3199b5e989f2
Markus Røyset <maroider@protonmail.com>
2020-04-20 06:19:09 +0200

    Be more paranoid about pipe name conficts

--- asbestos_shared/src/lib.rs
+++ asbestos_shared/src/lib.rs
@@ -1,5 +1,9 @@
 pub use named_pipe;
 
 pub fn named_pipe_name(pid: u32) -> String {
-    format!(r"\\.\pipe\{}-{}", env!("CARGO_PKG_NAME"), pid)
+    format!(
+        r"\\.\pipe\{}-{}",
+        concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION")),
+        pid
+    )
 }
